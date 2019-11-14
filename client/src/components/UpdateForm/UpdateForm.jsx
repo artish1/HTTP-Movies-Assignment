160fc1b8a8772a6754b65b3039d5d1132e85e699
Mark Artishuk <markyshuk@gmail.com>
2019-11-14 11:49:11 -0800

    Created update form and added routing to it

---
+++ client/src/components/UpdateForm/UpdateForm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Card, Typography } from "@material-ui/core";
+
+const UpdateForm = () => {
+  return (
+    <Card>
+      <Typography variant="h1" component="h1">
+        Update Movie
+      </Typography>
+      <form></form>
+    </Card>
+  );
+};
+
+export default UpdateForm;
